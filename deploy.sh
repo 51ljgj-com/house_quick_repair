7cf1c6f750c9e26be0ecc13a1010e4aab96298b9
caiping <caiping.cp@autonavi.com>
2018-12-16 20:26:34 +0800

    x

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 name="house_quick_repair.release_.tar.gz"
 rm -rf ./dist
-#npm run build
-./node_modules/webpack/bin/webpack.js --config build/webpack.dev.conf.js
+npm run build
+#./node_modules/webpack/bin/webpack.js --config build/webpack.dev.conf.js
 tar -zcf house_quick_repair.release_.tar.gz dist
 scp $name root@139.129.14.24:/var/www/house_repair_release/
 ssh root@139.129.14.24 "cd /var/www/house_repair_release ; tar -zxf $name"

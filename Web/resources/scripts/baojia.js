3b0c87f6ba8a875d5951dc31bc7122a85f856c5a
52cwalk <512014574@qq.com>
2019-06-12 19:28:21 +0800

    df

--- Web/resources/scripts/baojia.js
+++ Web/resources/scripts/baojia.js
@@ -4,6 +4,21 @@ var objectHeight = 0;
 var itemLength = 0;
 var itemWidth = 0;
 var currentObjectObj;
+
+var realLength = 0;
+
+var realWidth = 0;
+
+var tip_CalcYourself = "请自行计算尺寸 ！ ";
+var tip_PaperTypeError = "选择纸张类型不一致 ！ ";
+
+
+var composingItemPerfabObj;
+var composingCheckBoxPerfabObj;
+var composeArr = new Dictionary();
+
+var paperItemsArr = new Dictionary();
+
 function gradeChange(){
 
     var options=	$("#productTypeSelect");
@@ -23,15 +38,6 @@ function gradeChange(){
     });
 }
 
-function paperTypeChange(){
-    var options=	$("#paperTypeSelect");
-    var value = options.val();   //拿到选中项的值
-}
-
-function paperTypeChange(){
-    var options=	$("#paperTypeSelect");
-    var value = options.val();   //拿到选中项的值
-}
 
  function setDatabase(){
 
@@ -65,15 +71,14 @@ function paperTypeChange(){
 
         var divA = $(".realLength");
 
-        var realLength = itemLength+12;
+        realLength = itemLength+12;
 
-        var realWidth = itemWidth+12;
+        realWidth = itemWidth+12;
 
         $(".realLength").children('span').text('长度（实际）：'+realLength+'mm = '+itemLength+'(理论)+');
 
         $(".realWidth").children('span').text('宽度（实际）：'+realWidth+'mm = '+itemWidth+'(理论)+');
-   
-
+        
 /*
         divA.innerHTML = "长度（实际）1： 120mm = 100mm（理论）";
 
@@ -83,76 +88,252 @@ function paperTypeChange(){
  }
 
 var paperyItemPerfabObj;
+var paperItemId = 1;
 
- function addPaperItem()
+var currentSelectPaperL = 0;
+var currentSelectPaperW = 0;
+
+function addPaperItem()
  {
     if(objectLength == 0|| objectWidth == 0)
     {
         return;
     }
-
+    
      if(paperyItemPerfabObj == null)
      {
         paperyItemPerfabObj = $('.paperItem');
      }
 
-     paperyItemPerfabObj.clone(true).appendTo('#paperZone').show() ;
+    var newObj = paperyItemPerfabObj.clone(true).appendTo('#paperZone');
+
+     $(newObj).show();
+     $(newObj).addClass("newPaperItem");
+     $(newObj).attr("id","paperid_"+ paperItemId);
+     $(newObj).children('.leftNoDiv').text(paperItemId);
+     paperItemId++;
+
+     var pitem = new PaperItem();
+     pitem.targetObj =  newObj;
+     pitem.paperItemId =  $(newObj).attr("id");
+     pitem.realLength = realLength;
+     pitem.realWidth = realWidth;
+     pitem.objectLength = objectLength;
+     pitem.objectWidth = objectWidth;
+     pitem.objectHeight = objectHeight;
+     paperItemsArr.add($(newObj).attr("id"),pitem);
+
+     setAddcomposingItem(true);
+
  }
 
+ function addcomposingItem()
+ {
+    if(composingItemPerfabObj == null)
+    {
+        composingItemPerfabObj = $('.composingItem');
+    }
 
+    if(composingCheckBoxPerfabObj == null)
+    {
+        composingCheckBoxPerfabObj = $('.composeCheck');
+    }
+
+    var newObj = composingItemPerfabObj.clone(true).appendTo('#composingZone');
+    $(newObj).show();
+
+    $('.newPaperItem').each(function(){
+
+        var namestr = $(this).children('.leftNoDiv').text();
+        var itemid = $(this).attr('id');
+        var htmlobj = '	<input name='+ namestr + ' paperitemid= \''+ itemid +'\' type="checkbox" class="composeCheck" onclick=\"checkboxOnclick(this)\" value="1"> <label  for=\''+ namestr +'\'>'+ namestr  +'</label>'
+     //   var newcheckObj = composingCheckBoxPerfabObj.clone(true).appendTo($(newObj).children('.composingCenterDiv').children('.composingCheckBoxEditDiv'));
+     //   $(newcheckObj).show();
+     //  $(newcheckObj).text( $(this).children('.leftNoDiv').text());
+
+     //  e += "<input name = 'enable_a' type='checkbox' checked = 'checked' value= '1' style = 'margin: 6px 0px 0px 0px;width: 18px;height: 18px;'  onclick=\"checkboxOnclick(this,"+rowdata.id+")\" >";  
+
+      $(newObj).children('.composingCenterDiv').children('.composingCheckBoxEditDiv').append(htmlobj);
+    });
+ }
+
+//点击是否启用选择框
+function checkboxOnclick(obj){
+    
+    if(composeArr == null)
+    {
+        composeArr = new Dictionary();
+    }
+    else
+    {
+        composeArr.clear();
+    }
+
+    var paperitemids="";
+    var stetNo = 0;
+    $(obj).parent().children(":input").each(function(){
+        if (this.checked == true){
+            //Action for checked
+            var pid= $(this).attr("paperitemid");//获取id属性
+            composeArr.add(pid,true);
+            stetNo++;
+        }
+    });
+
+    var modeError = 0;
+    var sizeError = 0;
+    var length = 0;
+    var width = 0;
+    var paperMode = 0;
+    var index = 0;
+
+    for (var key in composeArr.datastore) {
+         var pitm1 = paperItemsArr.find(key);
+        
+        if(index == 0)
+        {
+            paperMode = pitm1.paperMode;
+            length = pitm1.realLength;
+            width = pitm1.realWidth;
+
+            currentSelectPaperL = length;
+            currentSelectPaperW = width;
+        }else
+        {
+            if(paperMode != pitm1.paperMode)
+            {
+                modeError = -1;
+            }
+
+            if(length != pitm1.realLength || width != pitm1.realWidth) {
+                sizeError = -1;
+            }
+        }
+        index++;
+    }
+
+    var tipObj= $(obj).parent().parent().children('.composeEditDiv').children('.tipDiv');
+    var editDivObj= $(obj).parent().parent().children('.composeEditDiv').children('.composeEditContentDiv');
+    //配件纸张实际宽(读取数据并显示)×竖排格(默认1)=排版理论宽(显示数据)
+    if(modeError == -1)
+    {
+        $(tipObj).text(tip_PaperTypeError);
+        $(tipObj).show();
+        $(editDivObj).hide();
+        currentSelectPaperL = 0;
+        currentSelectPaperW = 0;
+    }
+    else if(sizeError == -1)
+    {
+        $(tipObj).text(tip_CalcYourself);
+        $(tipObj).show();
+        $(editDivObj).hide();
+        currentSelectPaperL = 0;
+        currentSelectPaperW = 0;
+    }
+    else
+    {
+        if(index ==0)
+        {
+            $(tipObj).hide();
+            $(editDivObj).hide();
+            
+        }
+        else
+        {
+            var el = "配件纸张实际长 :"+ length+ "* <input class='chuxueInput' type='text' name='lname' value='1' onkeyup='updateLNo(this.value,this)'/>(横排/个)= <span> "+ length + "（理论长） </span>";  
+            var ew = "配件纸张实际宽 :"+ width+ "* <input class='chuxueInput' type='text' name='lname' value='1' onkeyup='updateWNo(this.value,this)'/>(横排/个)= <span> "+ width + "（理论宽） </span>";  
+
+            $(editDivObj).children('.composeEditSizeDiv').children('.paperLengthDiv').html(el);
+            $(editDivObj).children('.composeEditSizeDiv').children('.paperWidthDiv').html(ew);
+    
+            $(editDivObj).show();
+            $(tipObj).hide();
+        }
+    }
+}
+
+///纸张类型选择
+function paperTypeChange(obj){
+
+    var paperid =  $(obj).parent().parent().parent().parent().attr("id");
+   
+    var value = $(obj).val();   //拿到选中项的值
+    var pitm = paperItemsArr.find(paperid);
+    pitm.paperMode = value;
+}
+
+//更新血块自
  function updateRealb(b,obj)
  {
-     
-    obj.value =b.replace(/\D/g,'');
+    var paperid =  $(obj).parent().parent().parent().parent().attr("id");
 
-    var j = $(obj).parent().children('.juanbianInput').val();
-   var parentObjl = $(obj).parent();
+    var pitm = paperItemsArr.find(paperid);
     
-    if($(parentObjl).attr("class") == "realLength")
-    {
-        var theoreticalValue  = parseInt(objectLength)+objectHeight*2;
-        var real = itemLength+2*b+2*j;
+    pitm.updateRealblood(b,obj);
 
-        $(parentObjl).children('span').text('长度（实际）：'+real+'mm = '+theoreticalValue+'(理论)+');
-    }
-    else
-    {
-        var theoreticalValue  = parseInt(objectWidth)+objectHeight*2;
-        var real = itemWidth+2*b+2*j;
-        $(parentObjl).children('span').text('宽度（实际）：'+real+'mm = '+theoreticalValue+'(理论)+');
-    }
  }
 
+//更新卷边
  function updateRealj(j,obj)
  {
+    var paperid =  $(obj).parent().parent().parent().parent().attr("id");
 
-    obj.value =j.replace(/\D/g,'');
-
-    var b = $(obj).parent().children('.chuxueInput').val();
-  
-    var real = itemLength+2*b+2*j;
-
-    var parentObjl = $(obj).parent();
-    if($(parentObjl).attr("class") == "realLength")
-    {
-        var theoreticalValue  = parseInt(objectLength)+objectHeight*2;
-        var real = itemLength+2*b+2*j;
-        $(parentObjl).children('span').text('长度（实际）：'+real+'mm = '+theoreticalValue+'(理论)+');
-    }
-    else
-    {
-        var theoreticalValue  = parseInt(objectWidth)+objectHeight*2;
-        var real = itemWidth+2*b+2*j;
-        $(parentObjl).children('span').text('宽度（实际）：'+real+'mm = '+theoreticalValue+'(理论)+');
-    }
+    var pitm = paperItemsArr.find(paperid);
+    
+    pitm.updateRealjuan(j,obj);
  }
+
 
  function deletePaperItem(obj)
  {
     $(obj).parent().parent().remove();
+    if($(".paperItem").length <2 )
+    {
+        setAddcomposingItem(false);
+        deleteAllComposeItems();
+    }
+
+    paperItemsArr.remove($(obj).parent().parent().attr("id"));
  }
 
+ function updateLNo(v,obj)
+ {   
+    var el = "配件纸张实际长 :"+ currentSelectPaperL+ "* <input class='chuxueInput' type='text' name='lname' value='"+ v+ "' onkeyup='updateLNo(this.value,this)'/>(横排/个)= <span> "+ currentSelectPaperL*v + "（理论长）mm </span>";  
 
+    $(obj).parent().html(el);
+ }
 
+ function updateWNo(v,obj)
+ {
+    var ew = "配件纸张实际宽 :"+ currentSelectPaperW+ "* <input class='chuxueInput' type='text' name='lname' value='"+ v+ "' onkeyup='updateWNo(this.value,this)'/>(横排/个)= <span> "+ currentSelectPaperW*v  + "（理论宽）mm </span>";  
+
+    $(obj).parent().html(ew);
+ }
+
+ function setAddcomposingItem(v)
+ {
+    if(v)
+    {
+        $("#addcomposingItemBtn").show();
+    }
+    else
+    {
+        $("#addcomposingItemBtn").hide();
+    }
+ }
+
+ function deleteAllComposeItems()
+ {
+    $('.composingItem').each(function(){
+        $(this).remove();
+    });
+    
+ }
+
+ function showComposeTip(obj,str)
+ {
+    $(obj).text(str);
+ }
 
 
